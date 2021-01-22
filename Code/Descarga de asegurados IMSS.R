ef969ba54437e1825a3245ad54ab62d217da4106
vjvelascorios <vj.velascorios@gmail.com>
2021-01-22 03:52:04 -0600

    Update Descarga de asegurados IMSS.R

--- Code/Descarga de asegurados IMSS.R
+++ Code/Descarga de asegurados IMSS.R
@@ -1,13 +1,11 @@
 ######################## Descarga de datos de asegurados ########################
 #Paquetes
 rm(list=ls())
-pacman::p_load(downloader, tidyverse)
+pacman::p_load(downloader, tidyverse, RCurl)
 #Directorio general
-dir<-setwd("Z:\\Datos\\ASG IMSS")
+dir<-setwd("C:\\Users\\vjvelascorios\\Downloads/")
 #Se carga la tabla con las ligas para accesar a los datos
 links<-read_csv(url("https://raw.githubusercontent.com/vjvelascorios/Asegurados-IMSS/main/Utilities/Tabla_direcciones.csv"))
-
-
 ######################## 2002 ########################
 x<- "2002"
 links_mensuales<- links %>%
@@ -26,14 +24,5 @@ estension<-".csv" #extensión
 lista_general<-paste0(asg,year,estension)#el nombre de todos los archivos del año
 dir_completa_de_guardado<-paste0(y_i,"/",lista_general)#se crea la dirección completa donde se guardarán los archiuvos
 
-download("http://datos.imss.gob.mx/node/549/download",dir_completa_de_guardado[1])
-#Descarga de archivos
 download(as.character(links_mensuales[1,3]),dir_completa_de_guardado[1])
-
-
-download("http://datos.imss.gob.mx/node/549/download", "2002/asg-2002-01-01.csv") 
-
-
-download.file(url = "http://datos.imss.gob.mx/node/549/download", destfile = "2002/test.csv")
-
-download.
+|
