a664d216f9f12cc2aefe26404401666418020b99
vjvelascorios <vj.velascorios@gmail.com>
2023-04-14 14:06:26 -0600

    reading function changed due
    reading function changed due parsing problems

--- Code/Serializacion de datos en una carpeta.R
+++ Code/Serializacion de datos en una carpeta.R
@@ -13,17 +13,16 @@ serialized_files_names <- paste0(seq(as.Date("2022/1/1"), by = "month", length.o
 # Se indica el directorio donde se guardar�n los archivos serializados
 saving_diretory <- "/home/vjvelascorios/files/IMSS/qs_files/"
 # Se obtiene la ruta completa de los archivos objetivos a serializar (todos los archivos del 2020)
-target_files <- list.files(path = "/home/vjvelascorios/files/IMSS/2022/", full.names = T)
+target_files <- list.files(path = "/home/vjvelascorios/files/IMSS/2023/", full.names = T)
 
 
 # Se establece el loop el cual lee cada archivo en `target_files` y lo guarda con su respectivo nombre
 for (i in 1:length(target_files)) {
-  data <- read_csv(target_files[i], 
-                   locale = locale(encoding = "ISO-8859-1"))
+  data <- fread(target_files[i], encoding = "Latin-1")
   qsave(data,file = paste0(saving_diretory,serialized_files_names[i]))
 }
 
-# # Comprobaci�n de datos
+  # # Comprobaci�n de datos
 # enero_csv <- read_csv( "/home/vjvelascorios/files/IMSS/2022/asg-2022-01-31.csv",locale = locale(encoding = "ISO-8859-1"))
 # enero_qs <- qread("/home/vjvelascorios/files/IMSS/qs_files/2020-01-01.qs")
 # glimpse(enero_csv)
